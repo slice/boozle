a0b9167407bfa5eade5b055aa4e7b6059b0c52f0
Skip R <tinyslices@gmail.com>
2025-02-15 20:22:05 -0800

    don't construct `Discord[F]` in the app itself

--- src/main/scala/ski/ppy/mootiepop/main.scala
+++ src/main/scala/ski/ppy/mootiepop/main.scala
@@ -24,28 +24,29 @@ def commands[F[_]: Interaction] = Map[String, Cmd[F]](
 )
 
 object Main extends IOApp:
-  def app[F[_]](token: String)(using Async[F]): F[ExitCode] =
-    Discord.fromJDA[F](token).use: discord =>
-      for {
-        random <- Random.scalaUtilRandom[F]
-        events <- discord.events
-        _ <- events.subscribeUnbounded
-          .debug(event => s"[Debug]: Event: $event")
-          .collect:
-            case event @ Event.Slash(slash) =>
-              given Discord[F]     = discord
-              given Random[F]      = random
-              given Interaction[F] = Interaction.ofAsync[F](event)
-              Stream.eval:
-                commands[F].get(slash.getName).traverse: cmd =>
-                  cmd.run(cmd.args.extract(event).get).void
-                .void
-          .parJoinUnbounded
-          .compile
-          .drain
-      } yield ExitCode.Success
+  val token =
+    "MTI5ODc4ODk3MzUyNjkxMzA3NA.Gz0d1h.Y-TrM2kl_MTpE6i-hKOEd5Jk2USkhE0WrNMtYA"
+
+  def app[F[_]](using discord: Discord[F])(using Async[F]): F[ExitCode] =
+    for {
+      random <- Random.scalaUtilRandom[F]
+      events <- discord.events
+      _ <- events.subscribeUnbounded
+        .debug(event => s"[Debug]: Event: $event")
+        .collect:
+          case event @ Event.Slash(slash) =>
+            given Discord[F]     = discord
+            given Random[F]      = random
+            given Interaction[F] = Interaction.ofAsync[F](event)
+            Stream.eval:
+              commands[F].get(slash.getName).traverse: cmd =>
+                cmd.run(cmd.args.extract(event).get).void
+              .void
+        .parJoinUnbounded
+        .compile
+        .drain
+    } yield ExitCode.Success
 
   def run(args: List[String]): IO[ExitCode] =
-    app[IO](
-      "MTI5ODc4ODk3MzUyNjkxMzA3NA.Gz0d1h.Y-TrM2kl_MTpE6i-hKOEd5Jk2USkhE0WrNMtYA"
-    )
+    Discord.fromJDA[IO](token) use:
+      app(using _)
